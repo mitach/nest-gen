6ef4d6503068f5b577c5782969158abfbf3c8e69
Dimitar Malinov <malinovd02@gmail.com>
2025-05-28 11:06:37 +0300

    fix

--- client/src/app/generator/page.tsx
+++ client/src/app/generator/page.tsx
@@ -1,4 +1,5 @@
-import { ProjectGenerator } from "@/components/ProjectGenerator";
+// import { ProjectGenerator } from "@/components/ProjectGenerator";
+import { ProjectGenerator } from "../../components/ProjectGenerator";
 
 export default function NestJSGenerator() {
   return <ProjectGenerator />;
